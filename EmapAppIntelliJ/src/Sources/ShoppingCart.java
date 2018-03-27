06a11e3d4b3bdf40f73e5cf4f6b712226373b01c
DimaIonut <37832574+DimaIonut@users.noreply.github.com>
2018-03-27 13:29:55 +0300

    fixed checkout expection

--- EmapAppIntelliJ/src/Sources/ShoppingCart.java
+++ EmapAppIntelliJ/src/Sources/ShoppingCart.java
@@ -6,15 +6,31 @@ import java.util.List;
 public class ShoppingCart {
     public int Id;
 
-    public List<ShoppingCartItem> shoppingCartItem = new ArrayList<ShoppingCartItem> ();
+    public List <Item> shoppingCartItem = new ArrayList<> ();
 
-    public void addItem() {
+    public void addItem(Item item) {
+        shoppingCartItem.add(item);
     }
 
     public void checkOut() {
+        new OrderCommand().execute(shoppingCartItem);
+        shoppingCartItem.clear();
+        try{
+            if (this.Id <= 0);
+           }
+        catch (Exception exception)
+        {
+            System.out.println(exception + " You entered 0 or less");
+        }
     }
 
-    public void remove() {
+    public void remove(Item item) {
+
+        for(int i=0; i<shoppingCartItem.size(); i++)
+        {
+            if(shoppingCartItem.get(i).getId() == item.ID)
+                shoppingCartItem.remove(shoppingCartItem.get(i));
+        }
     }
 
 }
