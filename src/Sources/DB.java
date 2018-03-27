b36026605474e07962e659a6b41a591215ec5af7
Gheorghita Mutu <ghitamt@rocketmail.com>
2018-03-27 15:07:42 +0300

    added db changes from a team member

--- src/Sources/DB.java
+++ src/Sources/DB.java
@@ -1,21 +1,49 @@
 package Sources;
 
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DB {
-    public String name;
+    private static final String name = null;
 
-    public Date userList;
+    private static final List<User> userList = null;
 
-    public String itemList;
+    private static final List<User> listUsers = new ArrayList<User>();
+
+    private String itemList;
 
     public void getInstance() {
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    public static List<User> getUserList() {
+        return userList;
+    }
+
+    public static List<User> getListUsers() {
+        return listUsers;
+    }
+
+    public String getItemList() {
+        return itemList;
+    }
+
+    public void setItemList(String itemList) {
+        this.itemList = itemList;
     }
 
     public void getUsers() {
     }
 
     public void getItems() {
+    }
+
+    public void AddUser(User user1){
+        //Authentification auth = new Authentification();
+        //listUsers.add(auth.newUser());
     }
 
 }
